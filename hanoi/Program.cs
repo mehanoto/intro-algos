dd44fe7c622d9e23f561316aeef301115c87e953
stefancho <stefanstoqnov@gmail.com>
2022-11-03 21:29:52 +0200

    Hanoi recursive algorithm

---
+++ hanoi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hanoi
+{
+    class Program
+    {
+
+        public static void SolveHanoi(int n, char from, char to, char other) {
+
+            if (n == 1)
+            {
+                Console.WriteLine("Move disk from " + from + " to " + to);
+            }
+            else 
+            {
+                SolveHanoi(n - 1, from, other, to);
+                Console.WriteLine("Move disk from " + from + " to " + to);
+                SolveHanoi(n - 1, other, to, from);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SolveHanoi(3, 'a', 'b', 'c');
+            Console.ReadLine();
+        }
+    }
+}
